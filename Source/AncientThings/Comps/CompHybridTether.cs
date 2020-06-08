7c1075cb027c74edc851fd6580ab2b30e1b0230d
Damon <mdamon.connolly@gmail.com>
2020-06-08 10:44:00 +0100

    Ready to add the tether-comp

---
+++ Source/AncientThings/Comps/CompHybridTether.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using RimWorld;
+using System.Collections.Generic;
+using Verse;
+using Verse.AI;
+
+namespace AncientThings
+{
+    class CompHybridTether : ThingComp
+    {
+
+        public CompProperties_HybridTether Props
+        {
+            get
+            {
+                return (CompProperties_HybridTether)this.props;
+            }
+        }
+    }
+}
---
+++ Source/AncientThings/Comps/CompProperties/CompProperties_HybridTether.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace AncientThings
+{
+    class CompProperties_HybridTether : CompProperties
+    {
+
+        public CompProperties_HybridTether()
+        {
+            this.compClass = typeof(CompHybridTether);
+        }
+    }
+}
