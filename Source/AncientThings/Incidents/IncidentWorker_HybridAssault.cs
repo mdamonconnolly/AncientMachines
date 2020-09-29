198ee0a92afadf61dc45c82901951266ec563059
Damon <mdamon.connolly@gmail.com>
2020-09-29 19:36:24 +0100

    Fix skaut hunger stuff

---
+++ Source/AncientThings/Incidents/IncidentWorker_HybridAssault.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace AncientThings.Incidents
+{
+    class IncidentWorker_HybridAssault : IncidentWorker
+    {
+        protected override bool CanFireNowSub(IncidentParms parms)
+        {
+            return base.CanFireNowSub(parms);
+        }
+
+        protected override bool TryExecuteWorker(IncidentParms parms)
+        {
+            Map map = (Map)parms.target;
+            IntVec3 loc;
+            RCellFinder.TryFindRandomPawnEntryCell(out loc, map, CellFinder.EdgeRoadChance_Animal + 0.2f);
+
+            //Generate raider contents.
+            int hybridCount;
+
+            return true;
+        }
+    }
+}
